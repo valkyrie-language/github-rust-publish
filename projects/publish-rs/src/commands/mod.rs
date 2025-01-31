fad73bfdadd370b31a82e44f04b50884e2f0a460
Aster <galaster@foxmail.com>
2025-01-31 09:32:14 +0800

    Print the workspace

--- projects/publish-rs/src/commands/mod.rs
+++ projects/publish-rs/src/commands/mod.rs
@@ -1,12 +1,10 @@
-use crate::{
-    GithubError, bindings,
-    bindings::{Guest, export},
-};
-use std::{
-    path::{Path, PathBuf},
-};
-use std::env::VarError;
 use crate::bindings::GithubTarget;
+use crate::{
+    bindings, bindings::{export, Guest},
+    GithubError,
+};
+use std::collections::HashMap;
+use std::path::Path;
 
 pub struct RunningContext {}
 
@@ -14,8 +12,9 @@ impl Guest for RunningContext {
     fn run_with_config(config: String, target: GithubTarget) -> Result<(), GithubError> {
         match std::env::var("INPUT_CONFIG") {
             Ok(o) => {o}
-            Err(_) => {
-                println!("MissingConfig {}")
+            Err(e) => {
+                println!("MissingConfig {}", e);
+                return Ok(())
             }
         }
 
@@ -30,17 +29,41 @@ impl RunningContext {
     async fn run(&self, config: String) -> Result<(), GithubError> {
         match std::env::var("GITHUB_WORKSPACE") {
             Ok(s) => {
-                let config = PathBuf::from(s).join(&config);
-                let txt = std::fs::read_to_string(config);
-                println!("Config: {:?}", txt)
+                println!("GITHUB_WORKSPACE");
+                read_dir(&s)
             }
             Err(e) => {
                 println!("{}", e)
             }
         }
-        println!("Env: {:#?}", std::env::vars());
+        match std::env::var("RUNNER_WORKSPACE") {
+            Ok(s) => {
+                println!("RUNNER_WORKSPACE");
+                read_dir(&s)
+            }
+            Err(e) => {
+                println!("{}", e)
+            }
+        }
+        let mut envs = HashMap::new();
+        for (key, value) in std::env::vars() {
+            envs.insert(key, value);
+        }
+        println!("Env: {:#?}", envs);
         Ok(())
     }
 }
-
+fn read_dir(dir_path: &str) {
+    let path = Path::new(dir_path);
+    if let Ok(dir_entries) = std::fs::read_dir(path) {
+        for entry in dir_entries {
+            if let Ok(entry) = entry {
+                let file_name = entry.file_name();
+                println!("{}", file_name.to_string_lossy());
+            }
+        }
+    } else {
+        println!("Error reading directory: {}", dir_path);
+    }
+}
 export!(RunningContext with_types_in bindings);
